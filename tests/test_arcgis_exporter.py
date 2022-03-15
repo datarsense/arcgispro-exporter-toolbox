85a87e6214caf95b06d11fbff978ecc7be338f64
matt6697 <32440697+matt6697@users.noreply.github.com>
2022-03-15 19:13:34 +0100

    Delete sample KML file after test

--- tests/test_arcgis_exporter.py
+++ tests/test_arcgis_exporter.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from mock import patch, MagicMock
-import pandas as pd
 import xmltodict
+import os
 from src.arcgis_exporter import geojson_to_kml
 
 class TestArcgisExporter(TestCase):
@@ -32,13 +32,15 @@ class TestArcgisExporter(TestCase):
             ]
         }
 
-        geojson_to_kml(test_data, 'sample2.kml')
+        geojson_to_kml(test_data, 'sample.kml')
 
         #Test generated XML
         # Open JSON file
-        with open('sample2.kml', 'r') as resultkml:
+        with open('sample.kml', 'r') as resultkml:
             # Reading from json file
             resultdict = xmltodict.parse(resultkml.read())
 
             self.assertEqual(resultdict['kml']['Document']['Placemark'][0]['name'], 'Dinagat Islands')
             self.assertEqual(resultdict['kml']['Document']['Placemark'][0]['Point']['coordinates'], '125.6,10.1,0.0')
+
+        os.remove('sample.kml')
